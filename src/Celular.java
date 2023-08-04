57663b2a06e7a5fd5ff54808876dafa10c848fd2
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-08-03 21:50:13 -0300

    first commmit

---
+++ src/Celular.java
@@ -0,0 +1,48 @@
+
+public class Celular implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina() {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica() {
+
+    }
+}
